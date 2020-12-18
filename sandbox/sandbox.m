b1c9bc4711a67bb52323aa1ed402ed3c216cdba1
Philipp Schneider <schneiderp@mpi-magdeburg.mpg.de>
2020-12-18 14:30:50 +0100

    update

--- sandbox/sandbox.m
+++ sandbox/sandbox.m
@@ -1,25 +1,19 @@
-cnap = CNAloadNetwork({'../network_dirs/yield_net';1},1,1,0);
-r_P = 5;
-r_S = 6;
-r_BM = 7;
-modules = {struct};
-modules{1}.type = 'yield_w_constr';
-modules{1}.sense = 'target';
-% r*e/r*f <= y
-modules{1}.e = zeros(1,cnap.numr);
-modules{1}.e(r_P) = 1; % e numerator
-modules{1}.f = zeros(1,cnap.numr);
-modules{1}.f(r_BM) = 1; % f denominator
-modules{1}.y = 0;
-modules{1}.V = [];
-modules{1}.v = [];
-
-modules{2}.type = 'lin_constraints';
-modules{2}.sense = 'desired';
-modules{2}.V = zeros(1,cnap.numr);
-modules{2}.V(r_BM) = -1; % f denominator
-modules{2}.v = -0.5;
-
-obj = MCS_cplex(cnap,modules);
-[mcs,status] = obj.findSmallestMCS(maxSolutions,inf);
-CNAloadMCSinGUI(yield_net,-mcs);
+clear('cpx');
+cpx.f     = cnap.objFunc;
+cpx.sense = 'minimize';
+cpx.Aineq = modules{1}.V;
+cpx.bineq = modules{1}.v;
+cpx.Aeq   = cnap.stoichMat;
+cpx.beq   = zeros(cnap.nums,1);
+cpx.lb    = cnap.reacMin;
+cpx.ub    = cnap.reacMax;
+cpx = Cplex(cpx);
+cpx.Param.simplex.tolerances.feasibility.Cur = 0.00127291;
+x = cpx.solve();
+if x.status == 1
+    disp(x.x(find(cnap.objFunc)));
+    cnap.local.rb(:,1)=1:cnap.numr;
+    cnap.local.rb(:,2)=x.x;
+else
+    disp(x.statusstring);
+end
